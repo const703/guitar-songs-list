132db5e08de254e27223f569c8c938b35418dff6
Konstantin Ulyanov <const703@gmail.com>
2023-06-13 10:28:14 +0700

    Add a kebab popup menu to each song with an option to delete the song

--- app/src/main/java/com/konst/guitarsongslist/SongsAdapter.kt
+++ app/src/main/java/com/konst/guitarsongslist/SongsAdapter.kt
@@ -3,13 +3,16 @@ package com.konst.guitarsongslist
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.PopupMenu
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class SongsAdapter(private val songs: List<Song>) : RecyclerView.Adapter<SongsAdapter.SongViewHolder>() {
+class SongsAdapter(private val songs: ArrayList<Song>) : RecyclerView.Adapter<SongsAdapter.SongViewHolder>() {
 
     class SongViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val nameText: TextView = itemView.findViewById<TextView>(R.id.song_name_text)
+        val nameText: TextView = itemView.findViewById(R.id.song_name_text)
+        val moreButton: ImageButton = itemView.findViewById(R.id.more_button)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
@@ -19,6 +22,23 @@ class SongsAdapter(private val songs: List<Song>) : RecyclerView.Adapter<SongsAd
 
     override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
         holder.nameText.text = songs[position].name
+        holder.moreButton.setOnClickListener {
+            val popup = PopupMenu(holder.itemView.context, holder.moreButton)
+            popup.menuInflater.inflate(R.menu.song_menu, popup.menu)
+            popup.setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.action_delete -> {
+                        songs.removeAt(position)
+                        notifyItemRemoved(position)
+                        true
+                    }
+                    else ->{
+                        true
+                    }
+                }
+            }
+            popup.show()
+        }
     }
 
     override fun getItemCount(): Int {
