52e3d28c9b4f23720a1be6f785ce2fbfa54abafb
Konstantin Ulyanov <const703@gmail.com>
2023-06-14 23:22:37 +0700

    Add options to select the date when a song was last played, display the date in the song card

--- app/src/main/java/com/konst/guitarsongslist/SongsAdapter.kt
+++ app/src/main/java/com/konst/guitarsongslist/SongsAdapter.kt
@@ -1,5 +1,7 @@
 package com.konst.guitarsongslist
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,12 +9,26 @@ import android.widget.ImageButton
 import android.widget.PopupMenu
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.text.DateFormat
+import java.util.Calendar
+import java.util.Date
+
 
 class SongsAdapter(private val songs: ArrayList<Song>) : RecyclerView.Adapter<SongsAdapter.SongViewHolder>() {
 
     class SongViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val nameText: TextView = itemView.findViewById(R.id.song_name_text)
         val moreButton: ImageButton = itemView.findViewById(R.id.more_button)
+        private val lastPlayedText: TextView = itemView.findViewById(R.id.last_played_text)
+
+        fun setLastPlayed(lastPlayedDate: Date?) {
+            if (lastPlayedDate == null) {
+                lastPlayedText.text = "Last played: Never"
+            }
+            else {
+                lastPlayedText.text = "Last played: " + DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT).format(lastPlayedDate)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
@@ -22,6 +38,7 @@ class SongsAdapter(private val songs: ArrayList<Song>) : RecyclerView.Adapter<So
 
     override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
         holder.nameText.text = songs[position].name
+        holder.setLastPlayed(songs[position].lastPlayed)
         holder.moreButton.setOnClickListener {
             val popup = PopupMenu(holder.itemView.context, holder.moreButton)
             popup.menuInflater.inflate(R.menu.song_menu, popup.menu)
@@ -30,6 +47,44 @@ class SongsAdapter(private val songs: ArrayList<Song>) : RecyclerView.Adapter<So
                     R.id.action_delete -> {
                         songs.removeAt(position)
                         notifyItemRemoved(position)
+                        true
+                    }
+                    R.id.action_mark_as_just_played -> {
+                        songs[position].lastPlayed = Calendar.getInstance().time
+                        holder.setLastPlayed(songs[position].lastPlayed)
+                        true
+                    }
+                    R.id.action_choose_last_played_time -> {
+                        val calendar = Calendar.getInstance()
+
+                        var selectedDate = Date(0)
+                        val year = calendar.get(Calendar.YEAR)
+                        val month = calendar.get(Calendar.MONTH)
+                        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+                        val datePickerDialog = DatePickerDialog(holder.itemView.context,
+                            { _, selectedYear, selectedMonth, selectedDay ->
+                                selectedDate = Calendar.getInstance().apply {
+                                    set(selectedYear, selectedMonth - 1, selectedDay, 0, 0)
+                                }.time
+
+                                val timePickerDialog = TimePickerDialog(holder.itemView.context,
+                                    { _, selectedHour, selectedMinute ->
+                                        selectedDate = Date(selectedDate.time + 1000 * 60 * ((selectedHour * 60) + selectedMinute))
+
+                                        songs[position].lastPlayed = selectedDate
+                                        holder.setLastPlayed(songs[position].lastPlayed)
+                                    },
+                                    0, 0, false
+                                )
+                                timePickerDialog.show()
+
+
+                            }, year, month, day
+                        )
+                        datePickerDialog.datePicker.maxDate = System.currentTimeMillis()
+                        datePickerDialog.show()
+
                         true
                     }
                     else ->{
